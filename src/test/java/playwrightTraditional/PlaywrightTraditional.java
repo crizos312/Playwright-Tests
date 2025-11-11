a0275b6679ab8e9735dbf1803e35210a01c9ad85
Chris <crizos312@yahoo.com>
2025-11-11 01:12:25 -0600

    Playwright Testing test 1

---
+++ src/test/java/playwrightTraditional/PlaywrightTraditional.java
@@ -0,0 +1,118 @@
+package playwrightTraditional;
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
+import org.junit.jupiter.api.*;
+
+import java.nio.file.Paths;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class PlaywrightTraditional {
+
+    static Playwright playwright;
+    static Browser browser;
+    static BrowserContext context;
+    static Page page;
+
+    @BeforeAll
+    static void Test() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+                .setHeadless(false));
+
+        //Record the test
+        context = browser.newContext(new Browser.NewContextOptions()
+                        .setRecordVideoDir(Paths.get("videos/"))
+                        .setRecordVideoSize(1280, 720));
+
+        page = context.newPage();
+    }
+
+    @Test
+    void playwrightTest() {
+        //Bookstore
+        page.navigate("https://depaul.bncollege.com/");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Search")).click();
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Search")).fill("earbuds");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Search")).press("Enter");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("brand")).click();
+        page.getByRole(AriaRole.LISTITEM).filter(new Locator.FilterOptions().setHasText("brand JBL (12)")).getByRole(AriaRole.IMG).click();
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Color")).click();
+        page.getByRole(AriaRole.LISTITEM).filter(new Locator.FilterOptions().setHasText("Color Black (9)")).locator("svg").first().click();
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Price")).click();
+        page.locator("#facet-price svg").nth(2).click();
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("JBL Quantum True Wireless")).click();
+        assertThat(page.getByLabel("main").getByRole(AriaRole.HEADING)).containsText("JBL Quantum True Wireless Noise Cancelling Gaming Earbuds- Black");
+        assertThat(page.getByLabel("main")).containsText("sku 668972707");
+        assertThat(page.getByLabel("main")).containsText("$164.98");
+        assertThat(page.getByLabel("main")).containsText("Adaptive noise cancelling allows awareness of environment when gaming on the go. Light weight, durable, water resist. USB-C dongle for low latency connection < than 30ms.");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add to cart")).click();
+        assertThat(page.locator("#headerDesktopView")).containsText("1 items");
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Cart 1 items")).click();
+
+        //ShoppingCart Page
+        System.out.println("Current URL: " + page.url());
+        assertThat(page.getByLabel("main")).containsText("Your Shopping Cart");
+        assertThat(page.getByLabel("main")).containsText("JBL Quantum True Wireless Noise Cancelling Gaming Earbuds- Black");
+        assertThat(page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Quantity, edit and press"))).hasValue("1");
+        assertThat(page.getByLabel("main")).containsText("$164.98");
+        page.getByText("FAST In-Store PickupDePaul").click();
+        assertThat(page.getByLabel("main")).containsText("Subtotal $164.98");
+        assertThat(page.getByLabel("main")).containsText("Handling To support the bookstore's ability to provide a best-in-class online and campus bookstore experience, and to offset the rising costs of goods and services, an online handling fee of $3.00 per transaction is charged. This fee offsets additional expenses including fulfillment, distribution, operational optimization, and personalized service. No minimum purchase required. $3.00");
+        assertThat(page.getByLabel("main")).containsText("Taxes TBD");
+        assertThat(page.getByLabel("main")).containsText("Estimated Total $167.98");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Promo Code")).click();
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Enter Promo Code")).fill("TEST");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Apply Promo Code")).click();
+        assertThat(page.locator("#js-voucher-result")).containsText("The coupon code entered is not valid.");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Proceed To Checkout")).first().click();
+
+        //Account Page
+        assertThat(page.getByLabel("main")).containsText("Create Account");
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Proceed As Guest")).click();
+
+        //Contact Info
+        assertThat(page.getByLabel("main")).containsText("Contact Information");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("First Name (required)")).click();
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("First Name (required)")).fill("Chris");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("First Name (required)")).press("Tab");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Last Name (required)")).fill("Rizos");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Email address (required)")).click();
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Email address (required)")).fill("crizos@depaul.edu");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Phone Number (required)")).click();
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Phone Number (required)")).fill("2248174877");
+        assertThat(page.getByLabel("main")).containsText("$164.98");
+        assertThat(page.getByLabel("main")).containsText("Handling To support the bookstore's ability to provide a best-in-class online and campus bookstore experience, and to offset the rising costs of goods and services, an online handling fee of $3.00 per transaction is charged. This fee offsets additional expenses including fulfillment, distribution, operational optimization, and personalized service. No minimum purchase required. $3.00");
+        assertThat(page.getByLabel("main")).containsText("Tax TBD");
+        assertThat(page.getByLabel("main")).containsText("Total $167.98 167.98 $");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Continue")).click();
+
+        //Pickup info
+        assertThat(page.getByLabel("main")).matchesAriaSnapshot("- text: /Full Name Chris Rizos Email Address crizos@depaul\\.edu Phone Number \\+\\d+/");
+        assertThat(page.locator("#bnedPickupPersonForm")).containsText("Pickup Location DePaul University Loop Campus & SAIC 1 E. Jackson Boulevard, , Illinois, Chicago, 60604");
+        assertThat(page.locator("#bnedPickupPersonForm")).matchesAriaSnapshot("- text: I'll pick them up\n- radio \"I'll pick them up\" [checked]");
+        assertThat(page.getByLabel("main")).containsText("Order Subtotal $164.98");
+        assertThat(page.getByLabel("main")).containsText("Handling To support the bookstore's ability to provide a best-in-class online and campus bookstore experience, and to offset the rising costs of goods and services, an online handling fee of $3.00 per transaction is charged. This fee offsets additional expenses including fulfillment, distribution, operational optimization, and personalized service. No minimum purchase required. $3.00");
+        assertThat(page.getByLabel("main")).containsText("Tax TBD");
+        assertThat(page.getByLabel("main")).containsText("Total $167.98 167.98 $");
+        assertThat(page.getByLabel("main")).containsText("JBL Quantum True Wireless Noise Cancelling Gaming Earbuds- Black");
+        assertThat(page.getByLabel("main")).containsText("$164.98");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Continue")).click();
+
+        //Payment info
+        assertThat(page.getByLabel("main")).containsText("Order Subtotal $164.98");
+        assertThat(page.getByLabel("main")).containsText("Handling To support the bookstore's ability to provide a best-in-class online and campus bookstore experience, and to offset the rising costs of goods and services, an online handling fee of $3.00 per transaction is charged. This fee offsets additional expenses including fulfillment, distribution, operational optimization, and personalized service. No minimum purchase required. $3.00");
+        assertThat(page.getByLabel("main")).containsText("$17.22");
+        assertThat(page.getByLabel("main")).containsText("Total $185.20 185.2 $");
+        assertThat(page.getByLabel("main")).containsText("JBL Quantum True Wireless Noise Cancelling Gaming Earbuds- Black");
+        assertThat(page.getByLabel("main")).containsText("$164.98");
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Back to cart")).click();
+
+        //ShoppingCart again
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Remove product JBL Quantum")).click();
+        assertThat(page.locator("#headerDesktopView")).containsText("Cart 0 items");
+        browser.close();
+    }
+
+}
